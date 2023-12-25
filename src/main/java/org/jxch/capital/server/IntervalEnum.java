ab7d2dd94366db33719e63883b95bf0602c09728
jxch <qianbuhan@proton.me>
2023-12-25 09:33:28 +0800

    调整开发计划

--- src/main/java/org/jxch/capital/server/IntervalEnum.java
+++ src/main/java/org/jxch/capital/server/IntervalEnum.java
@@ -6,8 +6,19 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum IntervalEnum {
+    MINUTE_1("1m"),
+    MINUTE_2("2m"),
+    MINUTE_5("5m"),
+    MINUTE_15("15m"),
+    MINUTE_30("30m"),
+    MINUTE_60("60m"),
+    MINUTE_90("90m"),
+    HOUR_1("1h"),
+    HOUR_2("2h"),
+    HOUR_4("4h"),
     DAY_1("1d"),
     DAY_2("5d"),
+    WEEK_1("1wk"),
     MONTH_1("1mo"),
     MONTH_3("3mo"),
     MONTH_6("6mo"),
