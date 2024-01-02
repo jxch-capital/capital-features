9965b139d63c1b2ec786092d0aa0394f7dabfb18
jxch <qianbuhan@proton.me>
2024-01-03 07:01:14 +0800

    训练集简化 API

---
+++ src/main/java/org/jxch/capital/controller/TrainDataController.java
@@ -0,0 +1,41 @@
+package org.jxch.capital.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.jxch.capital.domain.dto.KNodeParam;
+import org.jxch.capital.domain.dto.KNodeTrains;
+import org.jxch.capital.server.TrainDataService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Collections;
+import java.util.List;
+
+@Slf4j
+@Controller
+@RequestMapping(path = "/data")
+@RequiredArgsConstructor
+public class TrainDataController {
+    private final TrainDataService trainDataService;
+
+    @ResponseBody
+    @RequestMapping("train")
+    public KNodeTrains trainData(@RequestBody KNodeParam param) {
+        return trainDataService.trainData(param);
+    }
+
+    @ResponseBody
+    @RequestMapping("prediction")
+    public KNodeTrains predictionData(@RequestBody KNodeParam param) {
+        return trainDataService.predictionData(Collections.singletonList(param));
+    }
+
+    @ResponseBody
+    @RequestMapping("predictions")
+    public KNodeTrains predictionData(@RequestBody List<KNodeParam> param) {
+        return trainDataService.predictionData(param);
+    }
+
+}
