1108716e8bea9b6809a0d98d20b94c3ba9d0fea3
jxch <qianbuhan@proton.me>
2024-01-08 18:28:54 +0800

    梳理开发进度

---
+++ src/main/java/org/jxch/capital/server/impl/MinkowskiIndicesKNNServiceImpl.java
@@ -0,0 +1,28 @@
+package org.jxch.capital.server.impl;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.jxch.capital.domain.dto.KLine;
+import org.jxch.capital.server.KNNService;
+import org.jxch.capital.server.KNNs;
+import org.springframework.stereotype.Service;
+import smile.math.distance.MinkowskiDistance;
+
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class MinkowskiIndicesKNNServiceImpl implements KNNService {
+    private final MinkowskiDistance minkowskiDistance = new MinkowskiDistance(2);
+
+    @Override
+    public double distance(List<KLine> a, List<KLine> b) {
+        return KNNs.distanceIndicesH(a, b, minkowskiDistance::d);
+    }
+
+    @Override
+    public String getName() {
+        return "闵可夫斯基距离-指标序列-横向";
+    }
+}
