7093fc7c79017c37143ea823fdc5d56fe2257642
jxch <qianbuhan@proton.me>
2024-01-14 06:43:42 +0800

    样式

---
+++ src/main/java/org/jxch/capital/notebook/TypeEnum.java
@@ -0,0 +1,16 @@
+package org.jxch.capital.notebook;
+
+public class TypeEnum {
+//    k: 单k线
+//    kn: k线组合
+//    kkh: 连接两根K线的高点
+//    kkl: 连接两根K线的低点
+//    kh: 高点水平线
+//    kl: 低点水平线
+//    kk2h: 双顶
+//    kk2l: 双底
+//    mark: 标记
+
+
+
+}
