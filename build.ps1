08e500275bd75fdf2b34923386489aeea3cee557
jxch <qianbuhan@proton.me>
2024-01-22 23:57:58 +0800

    docker buildx create --use

--- build.ps1
+++ build.ps1
@@ -1,1 +1,4 @@
 docker buildx build --platform=linux/arm64,linux/amd64 -t jxch/capital-features:$(Get-Date -Format 'yyyyMMddHH') -t jxch/capital-features:latest . --push
+
+# 第一次使用请先创建
+# docker buildx create --use
