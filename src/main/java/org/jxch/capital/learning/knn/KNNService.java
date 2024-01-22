fc963651980b6f1c5f0a1ad52f238be42175782c
jxch <qianbuhan@proton.me>
2024-01-23 00:50:06 +0800

    去除重复信号

--- src/main/java/org/jxch/capital/learning/knn/KNNService.java
+++ src/main/java/org/jxch/capital/learning/knn/KNNService.java
@@ -1,16 +1,20 @@
 package org.jxch.capital.learning.knn;
 
+import cn.hutool.core.date.DateUtil;
 import lombok.NonNull;
 import org.jxch.capital.domain.dto.KLine;
 import org.jxch.capital.domain.dto.KNeighbor;
 import org.jxch.capital.domain.dto.KNode;
 import org.jxch.capital.domain.dto.KNodeParam;
-import org.jxch.capital.stock.IntervalEnum;
 import org.jxch.capital.server.KNodeService;
+import org.jxch.capital.stock.IntervalEnum;
 import org.jxch.capital.utils.AppContextHolder;
+import org.jxch.capital.utils.CollU;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
 
 public interface KNNService {
 
@@ -27,20 +31,56 @@ public interface KNNService {
     }
 
     default List<KNeighbor> searchHasFutureNodes(KNode node, @NonNull List<KNode> all, int size, int futureNum) {
-        return all.stream()
+        List<KNeighbor> allNeighbors = all.stream()
                 .map(kn -> KNeighbor.builder()
                         .dist(distance(kn.subtractLast(futureNum).getKLines(),
                                 node.getKLines()))
                         .kNode(kn).build())
-                .sorted(Comparator.comparingDouble(KNeighbor::getDist))
-                .limit(size).toList();
+                .sorted(Comparator.comparingDouble(KNeighbor::getDist)).toList();
+
+        List<KNeighbor> neighbors = new ArrayList<>();
+        for (KNeighbor kNeighbor : allNeighbors) {
+            if (neighbors.size() < size) {
+                if (neighbors.isEmpty()) {
+                    neighbors.add(kNeighbor);
+                } else {
+                    KNeighbor last = CollU.last(neighbors);
+                    if (Objects.equals(last.getKNode().getCode(), kNeighbor.getKNode().getCode()) &&
+                            Math.abs(DateUtil.betweenDay(last.getKNode().getLastKLine().getDate(), kNeighbor.getKNode().getLastKLine().getDate(), true)) < kNeighbor.getKNode().getKLines().size()) {
+                        continue;
+                    }
+                    neighbors.add(kNeighbor);
+                }
+            } else {
+                break;
+            }
+        }
+        return neighbors;
     }
 
     default List<KNeighbor> search(KNode kNode, @NonNull List<KNode> all, int size) {
-        return all.stream().map(kn -> KNeighbor.builder()
+        List<KNeighbor> kNeighbors = all.stream().map(kn -> KNeighbor.builder()
                         .dist(distance(kn.getKLines(), kNode.getKLines())).kNode(kn).build())
-                .sorted(Comparator.comparingDouble(KNeighbor::getDist))
-                .limit(size).toList();
+                .sorted(Comparator.comparingDouble(KNeighbor::getDist)).toList();
+
+        List<KNeighbor> neighbors = new ArrayList<>();
+        for (KNeighbor kNeighbor : kNeighbors) {
+            if (neighbors.size() < size) {
+                if (neighbors.isEmpty()) {
+                    neighbors.add(kNeighbor);
+                } else {
+                    KNeighbor last = CollU.last(neighbors);
+                    if (Objects.equals(last.getKNode().getCode(), kNeighbor.getKNode().getCode()) &&
+                            Math.abs(DateUtil.betweenDay(last.getKNode().getLastKLine().getDate(), kNeighbor.getKNode().getLastKLine().getDate(), true)) < kNeighbor.getKNode().getKLines().size()) {
+                        continue;
+                    }
+                    neighbors.add(kNeighbor);
+                }
+            } else {
+                break;
+            }
+        }
+        return neighbors;
     }
 
     default List<KNeighbor> search(@NonNull KNodeParam kNodeParam, @NonNull KNodeService kNodeService, int size) {
