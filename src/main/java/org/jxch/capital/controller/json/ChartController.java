a9f6b018afd56a1654edab2c1bc733fbabf9e2b6
jxch <qianbuhan@proton.me>
2024-04-01 03:06:52 +0800

    格式化

--- src/main/java/org/jxch/capital/controller/json/ChartController.java
+++ src/main/java/org/jxch/capital/controller/json/ChartController.java
@@ -3,9 +3,11 @@ package org.jxch.capital.controller.json;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.jxch.capital.chart.ChartPngService;
 import org.jxch.capital.chart.dto.*;
 import org.jxch.capital.chart.impl.BreathChartServiceImpl;
 import org.jxch.capital.chart.impl.KLineChartServiceImpl;
+import org.jxch.capital.chart.impl.KNNBubbleChartServiceImpl;
 import org.jxch.capital.chart.impl.StockPoolBubbleChartServiceImpl;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -28,45 +30,42 @@ public class ChartController {
     private final KLineChartServiceImpl kLineChartService;
     private final BreathChartServiceImpl breathChartService;
     private final StockPoolBubbleChartServiceImpl stockPoolBubbleChartService;
+    private final KNNBubbleChartServiceImpl knnBubbleChartService;
 
     @SneakyThrows
     @PostMapping("kline")
     public ResponseEntity<StreamingResponseBody> kline(@RequestBody KLineChartParam param) {
-        KLineChartRes kLineChartRes = kLineChartService.chart(param);
-
-        return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(outputStream -> {
-            try (BufferedInputStream in = new BufferedInputStream(new FileInputStream(Paths.get(kLineChartRes.getPath()).toFile()))) {
-                StreamUtils.copy(in, outputStream);
-            } finally {
-                kLineChartService.clear(kLineChartRes);
-            }
-        });
+        KLineChartRes chartRes = kLineChartService.chart(param);
+        return commImage(kLineChartService, chartRes, chartRes.getPath());
     }
 
     @SneakyThrows
     @PostMapping("pool")
     public ResponseEntity<StreamingResponseBody> pool(@RequestBody StockPoolBubbleChartParam param) {
         StockPoolScatterChartRes chartRes = stockPoolBubbleChartService.chart(param);
+        return commImage(stockPoolBubbleChartService, chartRes, chartRes.getPath());
+    }
 
-        return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(outputStream -> {
-            try (BufferedInputStream in = new BufferedInputStream(new FileInputStream(Paths.get(chartRes.getPath()).toFile()))) {
-                StreamUtils.copy(in, outputStream);
-            } finally {
-                stockPoolBubbleChartService.clear(chartRes);
-            }
-        });
+    @SneakyThrows
+    @PostMapping("knn")
+    public ResponseEntity<StreamingResponseBody> knn(@RequestBody KNNBubbleChartParam param) {
+        KNNBubbleChartRes chartRes = knnBubbleChartService.chart(param);
+        return commImage(knnBubbleChartService, chartRes, chartRes.getPath());
     }
 
     @SneakyThrows
     @PostMapping("breath")
     public ResponseEntity<StreamingResponseBody> breath(@RequestBody BreathChartParam param) {
-        BreathChartRes breathChartRes = breathChartService.chart(param);
+        BreathChartRes chartRes = breathChartService.chart(param);
+        return commImage(breathChartService, chartRes, chartRes.getPath());
+    }
 
+    public <T extends ChartParam,R extends ChartRes> ResponseEntity<StreamingResponseBody> commImage(ChartPngService<T,R> service, R chartRes, String path) {
         return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(outputStream -> {
-            try (BufferedInputStream in = new BufferedInputStream(new FileInputStream(Paths.get(breathChartRes.getPath()).toFile()))) {
+            try (BufferedInputStream in = new BufferedInputStream(new FileInputStream(Paths.get(path).toFile()))) {
                 StreamUtils.copy(in, outputStream);
             } finally {
-                breathChartService.clear(breathChartRes);
+                service.clear(chartRes);
             }
         });
     }
